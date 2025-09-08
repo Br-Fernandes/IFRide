079756489e15244e95c3407a87f70d0a18646131
Rute Faria <faria.rute@Laptop-de-Rute.local>
2025-09-08 10:37:28 -0300

    creating your rides screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:if_ride/utils/themes.dart';
 import 'package:if_ride/views/screens/auth_screen.dart';
+import 'package:if_ride/views/screens/home_screen.dart';
 import 'package:if_ride/views/screens/main_navigation_screen.dart';
 import 'package:if_ride/views/screens/new_ride_screen.dart';
+import 'package:if_ride/views/screens/your_rides_screen.dart';
 import 'package:if_ride/views/widgets/steptwo_ride.dart';
 
 void main() {
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeColors.lightTheme,
-      home: NewRideScreen(),
+      home: YourRidesScreen(),
     );
   }
 }
--- lib/views/screens/your_rides_screen.dart
+++ lib/views/screens/your_rides_screen.dart
@@ -1,13 +1,87 @@
 import 'package:flutter/material.dart';
 
 class YourRidesScreen extends StatelessWidget {
-  const YourRidesScreen({super.key});
+  YourRidesScreen({super.key});
+
+  final ride = (
+    dateTime: DateTime(2025, 9,15, 9, 0),
+    origin: "Orizona",
+    destination: "IF Goiano"
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("TEla InicialTela suas carona"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Suas Caronas"),
+        centerTitle: false,
+      ),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(7),
+            color: Color(0xFFE8E7E7),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("${ride.dateTime}"),
+                Row(
+                  children: [
+                    Container(
+                      height: 12,
+                      width: 12,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        shape: BoxShape.circle
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Text("${ride.origin}"),
+                  ],
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  width: 12,
+                  color: Colors.red,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 12,
+                      width: 12,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        shape: BoxShape.circle
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Text("${ride.destination}"),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.047,
+                  width: MediaQuery.of(context).size.width * 0.5 ,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        Theme.of(context).primaryColor,
+                      ),
+                      foregroundColor: WidgetStatePropertyAll(Colors.white),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Detalhes",
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }

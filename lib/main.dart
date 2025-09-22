dc5740b33667548ca62ea7ce2df3966a31384fc9
Rute Faria <faria.rute@urt-aluno3.ifgoiano.local>
2025-09-22 16:26:33 -0300

    creating mock user for testing chat page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:if_ride/controllers/auth_controller.dart';
+import 'package:if_ride/models/chat.dart';
 import 'package:if_ride/utils/themes.dart';
 import 'package:if_ride/views/screens/auth_screen.dart';
+import 'package:if_ride/views/screens/chat_screen.dart';
 import 'package:if_ride/views/screens/conversations_screen.dart';
 import 'package:if_ride/views/screens/home_screen.dart';
 import 'package:if_ride/views/screens/main_navigation_screen.dart';
@@ -11,6 +13,8 @@ import 'package:if_ride/views/screens/your_rides_screen.dart';
 import 'package:if_ride/views/widgets/recurring_ride.dart';
 import 'package:if_ride/views/widgets/stepone_ride.dart';
 import 'package:if_ride/views/widgets/steptwo_ride.dart';
+
+import 'utils/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,10 +25,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final AuthController authController = Get.put(AuthController(), permanent: true);
+
+
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeColors.lightTheme,
-      home: ConversationsScreen(),
+      home: ChatScreen(chat: Chat(id: "jksdfhj", usersId: ['50f8153a-e1e0-48ba-b025-30a494788364', 'db4b6134-b9fd-41d3-8685-b390f7f9af63']),),
     );
   }
 }
@@ -34,7 +41,6 @@ class Initializer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final AuthController authController = Get.put(AuthController(), permanent: true);
   
     return Obx(() {
       if(authController.isAuthenticated) {
--- lib/views/screens/conversations_screen.dart
+++ lib/views/screens/conversations_screen.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:if_ride/controllers/chat_controller.dart';
-import 'package:if_ride/controllers/new_message_controller.dart';
-import 'package:if_ride/models/chat.dart';
-import 'package:if_ride/utils/constants.dart';
-import 'package:if_ride/views/screens/home_screen.dart';
-import 'package:if_ride/views/widgets/chat_widgets/message_bubble.dart';
 
 class ConversationsScreen extends StatelessWidget {
   const ConversationsScreen({super.key});
